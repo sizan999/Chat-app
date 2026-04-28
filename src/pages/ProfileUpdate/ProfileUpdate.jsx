b00f13411c40c02fc8beea9e209f0eb8a1a531c8
sizan <mdsizan120926.com>
2026-04-28 10:48:46 +0600

    profile update

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './ProfileUpdate.css'
+import assets from '../../assets/assets'
 
 const ProfileUpdate = () => {
   return (
-    <div></div>
+    <div className='profile'>
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id='avatar' accept='.png,.jpg,.jpeg' hidden/>
+            <img  src={assets.avatar_icon} alt="" />
+            upload profile image
+            </label>
+              <input type="text" placeholder='Your name' required />
+              <textarea placeholder='Write profile bio' required></textarea>
+              <button type='submit'>Save</button>
+        </form>
+        <img  className='profile-pic'src={assets.logo_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
